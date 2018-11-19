e6a39b015f671ddeb9c845da8cd26fd7c1921808
azertypow <azertypow@gmail.com>
2018-11-19 17:42:26 +0100

    bug: external_link_alumni return formated text

--- dist/site/config/api/Alumnis.php
+++ dist/site/config/api/Alumnis.php
@@ -25,16 +25,16 @@ class Alumnis
 
         foreach ($data->sortBy('date_end_alumni', 'desc') as $oneproject) {
             $json['alumnis'][] = array(
-                'url' => (string)$oneproject->url(),
-                'prenom_alumni' => (string)$oneproject->prenom_alumni(),
-                'nom_alumni' => (string)$oneproject->nom_alumni(),
-                'year' => (string)$oneproject->date_end_alumni(),
-                'actual_post_alumni' => (string)$oneproject->actual_post_alumni(),
-                'related_pp_alumni' => (string)$oneproject->related_pp_alumni(),
+                'url'                   => (string)$oneproject->url(),
+                'prenom_alumni'         => (string)$oneproject->prenom_alumni(),
+                'nom_alumni'            => (string)$oneproject->nom_alumni(),
+                'year'                  => (string)$oneproject->date_end_alumni(),
+                'actual_post_alumni'    => (string)$oneproject->actual_post_alumni(),
+                'related_pp_alumni'     => (string)$oneproject->related_pp_alumni(),
                 'related_thesis_alumni' => (string)$oneproject->related_thesis_alumni(),
-                'website_perso_alumni' => (string)$oneproject->website_alumni(),
-                'email_perso_alumni' => (string)$oneproject->email_alumni(),
-                'external_link_alumni' => (string)$oneproject->external_link_alumni()
+                'website_perso_alumni'  => (string)$oneproject->website_alumni(),
+                'email_perso_alumni'    => (string)$oneproject->email_alumni(),
+                'external_link_alumni'  => (string)$oneproject->external_link_alumni()->kirbytext(),
             );
         }
         return response::json($json);
