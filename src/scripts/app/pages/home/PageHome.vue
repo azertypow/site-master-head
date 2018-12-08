9149a7451a1f2ac1c2f4d952775332d16820b773
azertypow <azertypow@gmail.com>
2018-12-08 17:02:41 +0100

    chore: change $getProjectsPotionInSection to $getProjectsPlacedInSection function name

--- src/scripts/app/pages/home/PageHome.vue
+++ src/scripts/app/pages/home/PageHome.vue
@@ -76,7 +76,7 @@
             return this.homeProjects
         }
 
-        $getProjectsPotionInSection(sectionName: PERTINENCE_SECTION_NAME): IProjectsPositionInSection {
+        $getProjectsPlacedInSection(sectionName: PERTINENCE_SECTION_NAME): IProjectsPositionInSection {
             return {
                 header_position: this.getProjectAtPosition(sectionName, "header_position", this.$homeProjects),
                 middle_position: this.getProjectAtPosition(sectionName, "middle_position", this.$homeProjects),
@@ -97,7 +97,7 @@
                 fr: "Le designer crée ensuite les représentations, les formes faisant exister de façon identifiable l'esthétique du programme, du service ou d’un objet interactif connecté. Cette intervention nécessite la transformation de l'ensemble des éléments fonctionnels et des interactions du projet en objet appropriable par un utilisateur. Cette étape incarne l'existence et la raison du projet.",
                 en: "Usability answers the question \"can someone use this interface?\". Jacob Nielsen describes usability as the quality attribute [10] that describes how usable the interface is. Shneiderman proposes principles for designing more usable interfaces called \"Eight Golden Rules of Interface Design\"[11]—which are well-known heuristics for creating usable systems.",
             },
-            projects: this.$getProjectsPotionInSection(PERTINENCE_SECTION_NAME.WORKSHOP),
+            projects: this.$getProjectsPlacedInSection(PERTINENCE_SECTION_NAME.WORKSHOP),
         }}
 
         get $seminarsProjectsSectionData(): IProjectsSectionData {return {
@@ -109,7 +109,7 @@
                 fr: "Dans un projet numérique l'interface cristallise le potentiel, l'utilisation et la personnalité du produit, elle est à ce titre un enjeu de création et de différenciation qui est indissociable du produit dans son ensemble. L'interface est consubstantielle au produit numérique et l'aboutissement et l’agrégation du processus de design numérique. À ce titre, on peut avancer que le design interactif est aux actions interactives homme-machine-réseau-contenu, ce que le design produit est pour l'industrie manufacturière.",
                 en: "Alan Cooper argues in The Inmates Are Running the Asylum that we need a new approach to solving interactive software-based problems. The problems with designing computer interfaces are fundamentally different from those that do not include software (e.g., hammers). Cooper introduces the concept of cognitive friction, which is when the interface of a design is complex and difficult to use, and behaves inconsistently and unexpectedly, possessing different modes.",
             },
-            projects: this.$getProjectsPotionInSection(PERTINENCE_SECTION_NAME.SEMINAR),
+            projects: this.$getProjectsPlacedInSection(PERTINENCE_SECTION_NAME.SEMINAR),
         }}
 
         get $eventsProjectsSectionData(): IProjectsSectionData {return {
@@ -121,11 +121,11 @@
                 fr: "La société de l'information et la communication numérique définissent une nouvelle industrie et de nouveaux paradigmes économiques qui ont un impact direct sur le rôle du designer. Dans ce contexte, la question de l'offre et de la conception des produits prend un rôle central.",
                 en: "A persona encapsulates critical behavioural data in a way that both designers and stakeholders can understand, remember, and relate to. Personas use storytelling to engage users' social and emotional aspects, which helps designers to either visualize the best product behaviour or see why the recommended design is successful.",
             },
-            projects: this.$getProjectsPotionInSection(PERTINENCE_SECTION_NAME.EVENT),
+            projects: this.$getProjectsPlacedInSection(PERTINENCE_SECTION_NAME.EVENT),
         }}
 
         private sectionHasProjectsToShow(sectionName: PERTINENCE_SECTION_NAME): boolean {
-            const projectsPotion = this.$getProjectsPotionInSection(sectionName);
+            const projectsPotion = this.$getProjectsPlacedInSection(sectionName);
 
             if(projectsPotion.header_position) return true
             if(projectsPotion.middle_position) return true
