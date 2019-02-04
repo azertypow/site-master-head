9e46ad93dca138f295718dcf624c43df9aa5fdb3
azertypow <azertypow@gmail.com>
2019-02-04 12:37:55 +0100

    feat: get image size max information and php version

--- dist/site/config/api/Projects.php
+++ dist/site/config/api/Projects.php
@@ -31,6 +31,8 @@ class Projects
             })->toArray();
 
             $json['project'][] = array(
+                'max-size'              => Projects::getMaximumFileUploadSize(),
+                'version'              => phpversion(),
                 'uri'                   => $oneproject->uri(),
                 'url'                   => (string)$oneproject->url(),
                 'title'                 => (string)$oneproject->title(),
@@ -319,5 +321,53 @@ class Projects
         $pathForFinalGeneratedImage = $folderPathForGeneratedImages . $imageInProject['name'] . $imageParameter['extensionName'] . "." .$imageInProject['extension'];
 
         return $url . $pathForFinalGeneratedImage;
+    }
+
+    /**
+     * This function returns the maximum files size that can be uploaded
+     * in PHP
+     * @returns int File size in bytes
+     **/
+    public static function getMaximumFileUploadSize()
+    {
+        return min(Projects::convertPHPSizeToBytes(ini_get('post_max_size')), Projects::convertPHPSizeToBytes(ini_get('upload_max_filesize')));
+    }
+
+    /**
+     * This function transforms the php.ini notation for numbers (like '2M') to an integer (2*1024*1024 in this case)
+     *
+     * @param string $sSize
+     * @return integer The value in bytes
+     */
+    public static function convertPHPSizeToBytes($sSize)
+    {
+        //
+        $sSuffix = strtoupper(substr($sSize, -1));
+        if (!in_array($sSuffix,array('P','T','G','M','K'))){
+            return (int)$sSize;
+        }
+        $iValue = substr($sSize, 0, -1);
+        switch ($sSuffix) {
+            case 'P':
+                $iValue *= 1024;
+                break;
+            // Fallthrough intended
+            case 'T':
+                $iValue *= 1024;
+                break;
+            // Fallthrough intended
+            case 'G':
+                $iValue *= 1024;
+                break;
+            // Fallthrough intended
+            case 'M':
+                $iValue *= 1024;
+                break;
+            // Fallthrough intended
+            case 'K':
+                $iValue *= 1024;
+                break;
+        }
+        return (int)$iValue;
     }
 }
