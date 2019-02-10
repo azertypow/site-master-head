ead89261fbf1aa674e0578605175fcfb6eed1235
azertypow <azertypow@gmail.com>
2019-02-10 13:14:54 +0100

    chore: add static image on header

--- src/scripts/app/components/header/HeaderWithImage.vue
+++ src/scripts/app/components/header/HeaderWithImage.vue
@@ -2,9 +2,9 @@ import {LANG_LIST} from "../GLOBAL_ENUMS"
 <template>
     <section class="header-with-image app-header">
         <div class="app-header__background-container">
-            <div class="app-header__image" style="background-image: url('/content/projects/5-bootcamp/md_bootcamp-2018head-raphaellemueller-7-low.jpg');                transform: translateX(-145%)    translateY(-45%) translateZ(130px) rotateY(5deg)    rotateZ(-10deg);"></div>
-            <div class="app-header__image" style="background-image: url('/content/projects/5-bootcamp/md_bootcamp-2018head-raphaellemueller-8-low.jpg');                transform: translateX(-50%)     translateY(-50%) translateZ(125px) rotateY(-4deg)   rotateZ(7deg);"></div>
-            <div class="app-header__image" style="background-image: url('/content/projects/5-bootcamp/md_bootcamp-2018head-raphaellemueller-23-low.jpg');               transform: translateX(45%)      translateY(-50%) translateZ(120px) rotateY(5deg)    rotateZ(-4deg);"></div>
+            <div class="app-header__image" style="background-image: url('/content/projects/3-machine-learning-for-designers/generated/18_md_sdtlphead-raphaellemueller-84@mmd-regular.jpg');                transform: translateX(-145%)    translateY(-45%) translateZ(130px) rotateY(5deg)    rotateZ(-10deg);"></div>
+            <div class="app-header__image" style="background-image: url('/content/projects/3-machine-learning-for-designers/generated/18_md_sdtlphead-raphaellemueller-61@mmd-regular.jpg');                transform: translateX(-50%)     translateY(-50%) translateZ(125px) rotateY(-4deg)   rotateZ(7deg);"></div>
+            <div class="app-header__image" style="background-image: url('/content/projects/2-bootcamp/generated/md_bootcamp-2018head-raphaellemueller-11@mmd-regular.jpg');                                 transform: translateX(45%)      translateY(-50%) translateZ(120px) rotateY(5deg)    rotateZ(-4deg);"></div>
         </div>
         <div class="app-header__text-container">
             <div class="app-header__text">
@@ -89,9 +89,9 @@ import {LANG_LIST} from "../GLOBAL_ENUMS"
                 const vectorX = positionX / width
                 const vectorY = positionY / height
 
-                backgroundContainer.style.transform =   `rotateX(${vectorX / 2 * HeaderWithImage.degTransformation}deg) rotateY(${vectorY * HeaderWithImage.degTransformation}deg) translateX(${ - vectorX * HeaderWithImage.translationTransformation}px)`
+                backgroundContainer.style.transform =   `rotateX(${vectorX / 2 * HeaderWithImage.degTransformation}deg) rotateY(${vectorY * HeaderWithImage.degTransformation}deg)  translateX(${ - vectorX * HeaderWithImage.translationTransformation}px)`
 
-                textElement.style.transform =           `rotateX(${vectorY * HeaderWithImage.degTransformation}deg) rotateY(${ - vectorX * HeaderWithImage.degTransformation}deg) translateX(${ vectorX * HeaderWithImage.translationTransformation}px)`
+                textElement.style.transform =           `rotateX(${vectorY * HeaderWithImage.degTransformation}deg)     rotateY(${vectorX * HeaderWithImage.degTransformation}deg)  translateX(${ - vectorX * HeaderWithImage.translationTransformation}px)`
 
                 for(let index = 0; index < arrayOfImageElement.length; index++) {
 
